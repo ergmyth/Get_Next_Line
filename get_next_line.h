3dae098cef6cb27f267da09c8eec099a20bd5d04
Tinkotsu <eleonard@at-r2.21-school.ru>
2019-12-23 15:51:29 +0300

    Third Project 21

---
+++ get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eleonard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/15 11:34:14 by eleonard          #+#    #+#             */
+/*   Updated: 2019/09/20 18:48:16 by eleonard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 1
+
+# include "./libft/libft.h"
+
+int		get_next_line(const int fd, char **line);
+
+#endif
