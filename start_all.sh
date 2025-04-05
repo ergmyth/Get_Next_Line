9caa6f63db80c0b2b5724ae71670caac4f4e9cac
ARFatkullin <ARFatkullin@ufa.beeline.ru>
2025-04-05 20:05:57 +0500

    betboom

--- start_all.sh
+++ start_all.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # Kill previous launched executables
+
+rm nohup.out
 pkill -u root java
 pkill -u root chrome
 nohup java -jar coef_loader.jar &
 nohup java -jar server.jar &
+
 
 echo "Chef loader started."
 
